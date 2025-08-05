3b3bad87adb8cf5dd058ba48962a02869a4a0bfe
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-05 12:54:06 +0400

    Add login.

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -7,29 +7,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\File;
 use Illuminate\Validation\Rules\Password;
-use phpDocumentor\Reflection\DocBlock\Tags\Since;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('auth.register');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $userAttributes = $request->validate([
@@ -55,37 +40,5 @@ class RegisteredUserController extends Controller
         Auth::login($user);
 
         return redirect('/');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,9 +27,18 @@
                     <a href="">Companies</a>
                 </div>
 
-                <div>
-                    <a href="">Post a Job</a>
-                </div>
+                @auth
+                    <div>
+                        <a href="/jobs/create">Post a Job</a>
+                    </div>
+                @endauth
+
+                @guest
+                    <div class="px-4 space-x-6 font-bold">
+                        <a href="/register">Sign Up</a>
+                        <a href="/login">Login</a>
+                    </div>
+                @endguest
             </nav>
 
             <main class="mt-10 mx-auto max-w-[986px]">
